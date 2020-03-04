1804f76a92df841c604f98ff4f4b0cc885e56cac
Nikhil <csbnikhil@gmail.com>
2020-03-04 12:48:18 +0530

    More methods

--- elevator.py
+++ elevator.py
@@ -14,14 +14,16 @@ class Elevator:
         # TODO: Determine the next stop
         pass
 
-    def move(self):
+    def move(self, floor_num):
         # TODO: Get the next stop and move the lift.
         # It has to increment `current_floor` one at a time,
         # and have a delay so that another request can be added.
+        self.current_floor = floor_num
+        del self.requests[0]
 
-        pass
-
-    def press_button(floor_num):
+    def press_button(self, floor_num):
         self.active_buttons[floor_num] = not self.active_buttons[floor_num]
+        self.add_request(floor_num)
            
-    
+    def get_active_buttons(self):
+        return [i for i, status in enumerate(self.active_buttons) if status is True]
