50a493f0e912b6e1f391f285589afcbcf31142a0
Nikhil <csbnikhil@gmail.com>
2020-03-04 11:40:25 +0530

    Add more methods

--- elevator.py
+++ elevator.py
@@ -3,12 +3,26 @@ class Elevator:
         self.number = number
         self.requests = []
         self.active_buttons = [False for _ in range(10)]
-        self.max_weight = 1170
+        self.stops = [False for _ in range(10)]
+        # self.max_weight = 1170
+        self.current_floor = 0
         self.direction = None
 
     def add_request(self, floor_num):
         self.requests.append(floor_num)
     
-    def next_stop
-    
+    def next_stop(self):
+        # TODO: Determine the next stop
+        pass
+
+    def move(self):
+        # TODO: Get the next stop and move the lift.
+        # It has to increment `current_floor` one at a time,
+        # and have a delay so that another request can be added.
+
+        pass
+
+    def press_button(floor_num):
+        self.active_buttons[floor_num] = not self.active_buttons[floor_num]
+           
     
