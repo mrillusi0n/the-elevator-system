f0980ac961804ea086f21ea908b1e741e2c314a9
Nikhil CSB <csbnikhil@gmail.com>
2020-05-13 16:16:23 +0530

    Add NUM_FLOORS

--- elevator.py
+++ elevator.py
@@ -1,15 +1,19 @@
+# simulating the elevator system
+
+NUM_FLOORS = 10
+
 class Elevator:
     def __init__(self):
         self.requests = []
-        self.active_buttons = [False for _ in range(10)]
-        self.stops = [False for _ in range(10)]
+        self.active_buttons = [False for _ in range(NUM_FLOORS)]
+        self.stops = [False for _ in range(NUM_FLOORS)]
         # self.max_weight = 1170
         self.current_floor = 0
         self.direction = None
 
     def add_request(self, floor_num):
         self.requests.append(floor_num)
-    
+
     def next_stop(self):
         # TODO: Determine the next stop
         pass
@@ -24,6 +28,6 @@ class Elevator:
     def press_button(self, floor_num):
         self.active_buttons[floor_num] = not self.active_buttons[floor_num]
         self.add_request(floor_num)
-           
+
     def get_active_buttons(self):
         return [i for i, status in enumerate(self.active_buttons) if status is True]
